d5285f5c2e77576a34b7c4223ac855efa4b81af9
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2024-03-30 23:09:22 +0200

    Update SQLQuery1MOVIES.sql

--- SEMINAR 01/SQLQuery1MOVIES.sql
+++ SEMINAR 01/SQLQuery1MOVIES.sql
@@ -5,6 +5,7 @@ FROM STUDIO
 WHERE NAME='Disney'
   
 --2. Напишете заявка, която извежда рождената дата на актьора Jack Nicholson
+  
 SELECT BIRTHDATE
 FROM MOVIESTAR
 WHERE NAME='Jack Nicholson'
@@ -16,10 +17,13 @@ FROM STARSIN
 WHERE MOVIEYEAR=1980 OR MOVIETITLE ='Knight'
   
 --4. Напишете заявка, която извежда имената на продуцентите с нетниактиви над 10 000 000 долара
+  
 SELECT NAME
 FROM MOVIEEXEC
 WHERE NETWORTH>10000000
+  
 --5. Напишете заявка, която извежда имената на актьорите, които са мъже или живеят на Prefect Rd.
+  
 SELECT NAME
 FROM  MOVIESTAR
 WHERE GENDER='M' OR ADDRESS='Perfect Rd'
